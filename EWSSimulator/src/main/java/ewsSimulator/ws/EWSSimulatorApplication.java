f55a7d023c6485319667e47251d626998dacfa7a
Charmik Sheth - Software Developer <csheth@csheth-vm2.dwi.litle.com>
2018-08-03 10:43:32 -0400

    Initial commit

---
+++ EWSSimulator/src/main/java/ewsSimulator/ws/EWSSimulatorApplication.java
@@ -0,0 +1,13 @@
+package ewsSimulator.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWSSimulatorApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(EWSSimulatorApplication.class, args);
+    }
+}
