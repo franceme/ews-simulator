32ff8271791893bb7da3e1c3080804049776cd32
Dave kartik - dev <kdave@kdave-vm2.dwi.litle.com>
2018-08-06 11:37:40 -0400

    Add registration Request

--- EWSSimulator/src/main/java/ewsSimulator/ws/EWSSimulatorEndpoint.java
+++ EWSSimulator/src/main/java/ewsSimulator/ws/EWSSimulatorEndpoint.java
@@ -4,6 +4,8 @@ import ewsSimulator.ws.generated.BatchDetokenizeRequest;
 import ewsSimulator.ws.generated.BatchDetokenizeResponse;
 import ewsSimulator.ws.generated.EchoRequest;
 import ewsSimulator.ws.generated.EchoResponse;
+import ewsSimulator.ws.generated.RegistrationRequest;
+import ewsSimulator.ws.generated.RegistrationResponse;
 
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -27,5 +29,17 @@ public class EWSSimulatorEndpoint {
     @ResponsePayload
     public EchoResponse echo(@RequestPayload EchoRequest echoRequest) {
         return new EchoResponse();
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "RegistrationRequest")
+    @ResponsePayload
+    public RegistrationResponse registration(@RequestPayload RegistrationRequest registrationRequest) {
+        RegistrationResponse response = new RegistrationResponse();
+        response.setRequestId("417d3d96-0675-4306-be5b-652c8c6ba9f7");
+        response.setRegId("1479321767965480923");
+        response.setToken("1111000100038566");
+        response.setTokenNewlyGenerated(false);
+
+        return response;
     }
 }
