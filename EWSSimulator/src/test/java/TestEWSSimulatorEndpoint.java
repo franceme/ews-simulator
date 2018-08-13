4485c512ce37920d890593d57ef75ee494af9056
Charmik Sheth - Software Developer <csheth@csheth-vm2.dwi.litle.com>
2018-08-13 10:06:00 -0400

    Added tokenInquiry exception test and updated tokenize exception test to use merchant ref id

--- EWSSimulator/src/test/java/TestEWSSimulatorEndpoint.java
+++ EWSSimulator/src/test/java/TestEWSSimulatorEndpoint.java
@@ -197,10 +197,11 @@ public class TestEWSSimulatorEndpoint {
     @Test
     public void testTokenize_Exception() {
         TokenizeRequest request = new TokenizeRequest();
+        request.setMerchantRefId("112233002");
         MerchantType merchant = new MerchantType();
         merchant.setRollupId(rollupId);
         request.setMerchant(merchant);
-        request.setPrimaryAccountNumber("615348948648002");
+        request.setPrimaryAccountNumber("615348948648468");
 
         try{
             TokenizeResponse response = ewsSimulatorEndpoint.tokenize(request);
@@ -234,5 +235,29 @@ public class TestEWSSimulatorEndpoint {
         List<Token> tokens = response.getToken();
         assertNotNull(tokens.get(0));
         assertNull(tokens.get(1));
+    }
+
+    @Test
+    public void testTokenInquiry_Exception() {
+        TokenInquiryRequest request = new TokenInquiryRequest();
+        request.setMerchantRefId("112233002");
+        MerchantType merchant = new MerchantType();
+        merchant.setRollupId(rollupId);
+        request.setMerchant(merchant);
+        List<Card> cards = request.getCard();
+
+        Card card1 = new Card();
+        card1.setPrimaryAccountNumber("1234567891011123");
+        cards.add(card1);
+
+        try {
+            TokenInquiryResponse response = ewsSimulatorEndpoint.tokenInquiry(request);
+            fail("ServerFaultException expected. None thrown");
+        } catch (ServerFaultException ex) {
+            ServerFault serverFault = ex.getServerFault();
+            assertEquals(2, (int) serverFault.getId());
+            assertEquals("UNKNOWN_ERROR", serverFault.getCode());
+            assertEquals("an unspecified error occurred.", serverFault.getMessage());
+        }
     }
 }
