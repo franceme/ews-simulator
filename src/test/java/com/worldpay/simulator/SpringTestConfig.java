647284aca2fdef709107da5dcc589c3c15dddf96
davekartik24 <davekartik24@gmail.com>
2018-08-15 14:08:24 -0400

    AutoWired HttpHeaderUtils class and refected it in Endpoint class

---
+++ src/test/java/com/worldpay/simulator/SpringTestConfig.java
@@ -0,0 +1,22 @@
+package com.worldpay.simulator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.worldpay.simulator.utils.HttpHeaderUtils;
+
+@ContextConfiguration
+public class SpringTestConfig {
+
+    @Bean
+    public HttpHeaderUtils httpHeaderUtils(){
+        HttpHeaderUtils httpHeaderUtils = new HttpHeaderUtils();
+        return httpHeaderUtils;
+    }
+
+    @Bean(name = "testEWSSimulator")
+    public EWSSimulatorEndpoint eWSSimulatorEndpoint(){
+        EWSSimulatorEndpoint eWSSimulatorEndpoint = new EWSSimulatorEndpoint();
+        return eWSSimulatorEndpoint;
+    }
+}
