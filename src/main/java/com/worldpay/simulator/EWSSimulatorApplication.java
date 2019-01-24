dd85393c8910a40469fdaf3ba67fa7bc35cb5799
fletchertime <nfletcher.mailbox@gmail.com>
2019-01-24 16:11:33 -0500

    Fixed build.gradle

--- src/main/java/com/worldpay/simulator/EWSSimulatorApplication.java
+++ src/main/java/com/worldpay/simulator/EWSSimulatorApplication.java
@@ -2,9 +2,16 @@ package com.worldpay.simulator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EWSSimulatorApplication {
+public class EWSSimulatorApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EWSSimulatorApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EWSSimulatorApplication.class, args);
